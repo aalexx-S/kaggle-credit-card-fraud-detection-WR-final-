39fb3d6029b443c6373e44aecd1530b7c083cb53
aalexx-S <alex530519@gmail.com>
2017-06-17 19:33:06 +0800

    fix oversample naive empty return

--- oversample.py
+++ oversample.py
@@ -7,8 +7,10 @@ def over_sampling_SMOTE_imblearn(X, y, kind):
 
 def over_sampling_naive(X, y, ratio):
     pool = [X[i] for i in range(len(y)) if y[i] == 1]
-    size = len(pool)
-    tmp = []
-    for i in range(size):
-        tmp.append(pool[random.randint(0, len(pool)-1)])
-    return X.extend(tmp), y.extend([1]*size)
+    size = int(len(pool) * (ratio-1))
+    tmp = [random.choice(pool) for i in range(size)]
+    reX = X.copy()
+    rey = y.copy()
+    reX.extend(tmp)
+    rey.extend([1]*size)
+    return reX, rey
