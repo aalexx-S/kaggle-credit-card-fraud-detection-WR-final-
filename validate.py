e139b188c742abd8fe5a9c44e48b5d89631d4d7b
aalexx-S <alex530519@gmail.com>
2017-06-17 20:38:39 +0800

    Add truth table.
    Add truth table calculate and printing method.

--- validate.py
+++ validate.py
@@ -6,3 +6,15 @@ def correction(answer, target):
         if i == j:
             cor += 1
     return float(cor) / len(answer)
+
+def truth_table(answer, target):
+    a = {0:0, 10:0, 1:0, 11:0}
+    for i, j in zip(answer, target):
+        a[10*i + j] += 1
+    return a
+
+def print_truth_table(table):
+    print('ans\pred   True     False  ')
+    print('===========================')
+    print('True  =     {0}      {1}'.format(table[11], table[10]))
+    print('False =     {0}      {1}'.format(table[1], table[0]))
