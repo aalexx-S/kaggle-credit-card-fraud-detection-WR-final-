ba558c766ec4bac848cfc524e12fea47faadc09a
b02902052 <b02902052@linux11.csie.ntu.edu.tw>
2017-06-22 15:13:45 +0800

    Add F score

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -2,5 +2,5 @@ all = ['read_data', 'valicut', 'validate']
 
 from .read_data import read_data
 from .valicut import valicut
-from .validate import correction, truth_table, print_truth_table
+from .validate import correction, truth_table, print_truth_table, f1_score
 from .featureSelection import get_feature_selector, get_index
