0da043d16b52f917945a8459cdc1d49a89fff996
aalexx-S <alex530519@gmail.com>
2017-06-22 23:31:57 +0800

    add pySMOTE to package-setup.sh. Oops.

--- package-setup.sh
+++ package-setup.sh
@@ -3,3 +3,8 @@
 git clone https://github.com/scikit-learn-contrib/imbalanced-learn.git
 cd imbalanced-learn
 pip install .
+cd ..
+
+git clone https://github.com/akazs/pySMOTE.git
+cd pySMOTE
+pip install .
