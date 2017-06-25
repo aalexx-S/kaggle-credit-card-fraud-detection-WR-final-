db789a9220c517782b6b2354ada683168f422ed5
aalexx-S <alex530519@gmail.com>
2017-06-25 21:27:21 +0800

    Fix naiveUS act like an idiot...
    Fix naiveUS not checking major class.
    I am so tired...

--- Sampler/naiveUS.py
+++ Sampler/naiveUS.py
@@ -1,17 +1,34 @@
 from numpy.random import choice
 from numpy import array
+from numpy import concatenate
+from collections import defaultdict
 
 from .baseSampler import BaseSampler
 
 class NaiveUS(BaseSampler):
     """NaiveUS: naive under sampler
+    only undersample the majority class
     """
     def __init__(self, config):
         self.ratio = int(config.get('SAMPLER','ratio'))
 
     def fit_sample(self, X, y):
-        size = int(len(X) / self.ratio)
+        # choose label
+        tmpd = defaultdict(lambda: 0)
+        for i in y:
+            tmpd[i] += 1
+        label = None
+        maxt = -float('inf')
+        for i, j in tmpd.items():
+            if j > maxt:
+                maxt = j
+                label = i
+        # sample
+        pool = [i for i, j in zip(X, y) if j == label]
+        otherX, othery = zip(*[(i, j) for i, j in zip(X, y) if j != label])
+        size = int(len(pool) / self.ratio)
         if size == 0:
             size = 1
-        index = choice(range(len(X)), size, False)
-        return array(X)[index], array(y)[index]
+        index = choice(range(len(pool)), size, False)
+        return concatenate((array(pool)[index], list(otherX))),\
+                            concatenate(([label]*size, list(othery)))
